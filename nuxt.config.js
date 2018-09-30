7132ef52bede0a3dc52d388823d3ad97b200e976
takenakasuji <t.nksj.e.89@gmail.com>
2018-10-01 00:41:03 +0900

    change env

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,11 @@
-const config = require('./.contentful.json')
+const {getConfigForKeys} = require('./lib/config.js')
+const ctfConfig = getConfigForKeys([
+  'CTF_BLOG_POST_TYPE_ID',
+  'CTF_SPACE_ID',
+  'CTF_CDA_ACCESS_TOKEN'
+])
+const {createClient} = require('./plugins/contentful')
+const cdaClient = createClient(ctfConfig)
 
 module.exports = {
   /*
@@ -37,11 +44,20 @@ module.exports = {
       }
     }
   },
+  generate: {
+    routes () {
+      return cdaClient.getEntries({
+        'content_type': ctfConfig.CTF_BLOG_POST_TYPE_ID
+      }).then(entries => {
+        return [
+          ...entries.items.map(entry => `/posts/${entry.fields.slug}`)
+        ]
+      })
+    }
+  },
   env: {
-    CTF_SPACE_ID: config.CTF_SPACE_ID,
-    CTF_CDA_ACCESS_TOKEN: config.CTF_CDA_ACCESS_TOKEN,
-    CTF_PERSON_ID: config.CTF_PERSON_ID,
-    CTF_BLOG_POST_TYPE_ID: config.CTF_BLOG_POST_TYPE_ID
+    CTF_SPACE_ID: ctfConfig.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: ctfConfig.CTF_CDA_ACCESS_TOKEN,
+    CTF_BLOG_POST_TYPE_ID: ctfConfig.CTF_BLOG_POST_TYPE_ID
   }
 }
-
